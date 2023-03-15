4ecf247caaebad92b20a60b296acf52b0296bfb6
Neptune102489 <skyblue1000knkn@gmail.com>
2023-03-15 20:10:17 +0900

    station15のリファクタリング

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -2,16 +2,18 @@ function displayList() {
   const f = document.getElementById('fruits');
   const p = document.querySelectorAll('p');
 
-  while (f.firstChild) {
+  for (let i = 0; i < p.length * 2; i++) { // <p>とその前にあるスペースが子要素になるため*2とする
+    if (f.firstChild) {
       f.removeChild(f.firstChild);
     }
-    
+  }
+
   const newUl = document.createElement('ul');
   f.appendChild(newUl);
   
   p.forEach(p_element => {
-      const li = document.createElement('li');
-      li.textContent = p_element.textContent;
-      document.querySelector('ul').appendChild(li);
-    });
+    const li = document.createElement('li');
+    li.textContent = p_element.textContent;
+    document.querySelector('ul').appendChild(li);
+  });
 }
